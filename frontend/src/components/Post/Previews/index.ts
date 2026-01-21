13d55327d5d3f06f46c6dc1590b25aa1b8e190d6
AchuAshwath <achuashwath88@gmail.com>
2026-01-21 17:17:02 +0530

    feat: add linkedin and x post previews

---
+++ frontend/src/components/Post/Previews/index.ts
@@ -0,0 +1,3 @@
+export { LinkedInPostPreview, type PreviewPostData } from "./LinkedInPostPreview"
+export { XPostPreview } from "./XPostPreview"
+export { PostPreviewDialog } from "./PostPreviewDialog"
